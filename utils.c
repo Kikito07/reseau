62a48b17abb3f2bd1d5470dbb60a1cfe153d5f3c
Arnaud <arnaudontour@gmail.com>
2019-10-19 11:34:43 +0200

    timestamp

--- utils.c
+++ utils.c
@@ -3,8 +3,11 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <sender_list.h>
+#include <time.h>
 
-int real_address(char *address, struct sockaddr_in6 *rval) {
+int real_address(char *address, struct sockaddr_in6 *rval)
+{
   struct addrinfo hints, *result;
 
   memset(&hints, 0, sizeof(hints));
@@ -14,23 +17,27 @@ int real_address(char *address, struct sockaddr_in6 *rval) {
   hints.ai_protocol = 0;
 
   int err = getaddrinfo(address, NULL, &hints, &result);
-  if (err != 0) {
+  if (err != 0)
+  {
     return -1;
   }
   memcpy(rval, result->ai_addr, result->ai_addrlen);
   return 0;
 }
 
-int read_file_and_send(char *filename, int sock) {
+int read_file_and_send(char *filename, int sock)
+{
   int seqn = 0;
   // opening file
-  if (filename == NULL) {
+  if (filename == NULL)
+  {
     printf("filename is NULL\n");
     return -1;
   }
   // opening file
   int fd = open(filename, O_RDWR);
-  if (fd < 0) {
+  if (fd < 0)
+  {
     printf("fail to open\n");
     return -1;
   }
@@ -38,59 +45,72 @@ int read_file_and_send(char *filename, int sock) {
   struct stat buf;
   fstat(fd, &buf);
   int sz = buf.st_size;
-  if (sz == 0) {
+  if (sz == 0)
+  {
     close(fd);
     return -1;
   }
   // initializing variables
-  pkt_t *pkt = pkt_new();
+
   char pkt_encoded[528];
   char buf_payload[512];
   int bytes_r;
+  int list_err;
+  list_t *list = list_init();
 
   // reading file and sending it
-  while (sz >= 512) {
+  while (sz >= 512)
+  {
     bytes_r = read(fd, buf_payload, 512);
-    if(bytes_r == -1){
+    if (bytes_r == -1)
+    {
       printf("read fail\n");
       return -1;
     }
+    pkt_t *pkt = pkt_new();
     pkt_set_payload(pkt, buf_payload, 512);
-    pkt_set_seqnum(pkt, 0);
-    pkt_set_timestamp(pkt, 0);
+    pkt_set_seqnum(pkt, seqn);
+    pkt_set_timestamp(pkt, time(NULL));
     sz -= 512;
     size_t encoded_byte = 528;
-    if (pkt_encode(pkt, pkt_encoded, &encoded_byte) != 0) {
+    if (pkt_encode(pkt, pkt_encoded, &encoded_byte) != 0)
+    {
       printf("encode error\n");
       close(fd);
       return -1;
     }
     ssize_t byte_sent;
-    byte_sent = send(sock, pkt_decode, encoded_byte, 0);
-    if (byte_sent == -1) {
+    list_err = push(list, pkt);
+    if (list_err == -1)
+    {
       close(fd);
       return -1;
     }
+    seqn = (seqn + 1) % 256;
   }
-  if (sz > 0) {
+  if (sz > 0)
+  {
     bytes_r = read(fd, buf_payload, sz);
-    if(bytes_r == -1){
+    if (bytes_r == -1)
+    {
       printf("read fail\n");
       return -1;
     }
+    pkt_t *pkt = pkt_new();
     pkt_set_payload(pkt, buf_payload, sz);
     pkt_set_seqnum(pkt, seqn);
-    seqn++;
-    pkt_set_timestamp(pkt, 0);
+    pkt_set_timestamp(pkt, time(NULL));
     size_t encoded_byte = 528;
-    if (pkt_encode(pkt, pkt_encoded, &encoded_byte) != 0) {
+    if (pkt_encode(pkt, pkt_encoded, &encoded_byte) != 0)
+    {
       printf("encode error\n");
       close(fd);
       return -1;
     }
     ssize_t byte_sent;
-    byte_sent = send(sock, pkt_encoded, encoded_byte, 0);
-    if (byte_sent == -1) {
+    list_err = push(list, pkt);
+    if (list_err == -1)
+    {
       close(fd);
       return -1;
     }
